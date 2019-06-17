e00e238dafe51b1c9c3b80ae3886a03b32afe0d4
Fernando <feh.wilinando@gmail.com>
2019-06-17 18:26:26 -0300

    Improving zookeeper configuration for kafka

--- assets/docker-entrypoint.sh
+++ assets/docker-entrypoint.sh
@@ -11,17 +11,30 @@ requireNonNull() {
     fi
 }
 
+removeZookeeperChrootFrom() {
+    echo $1 | sed -e 's/\/.*$//g'
+}
+
+waitingForFirstResponseFrom() {
+    servers=$1
+
+    IFS=',' #Internal Field Separator
+
+    for zookeeper_server in $servers; do 
+        echo "Waiting 10 seconds for $zookeeper_server"
+        wait-for-it $zookeeper_server -q --strict -t 10 -- kafka-server-start.sh config/server.properties && break
+    done
+}
+
 envsubst < /etc/kafka/config/server.properties > /opt/kafka/config/server.properties
 
 if [ -z "$1" ]; then    
-    
-    requireNonNull "ZOOKEEPER_HOST"
-    requireNonNull "ZOOKEEPER_PORT"
+    requireNonNull "ZOOKEEPER_SERVERS"    
     requireNonNull "ADVERTISE_HOST"
 
-    wait-for-it $ZOOKEEPER_HOST:$ZOOKEEPER_PORT --strict -t 10 -- kafka-server-start.sh config/server.properties
+    all_zookeeper_servers=$(removeZookeeperChrootFrom $ZOOKEEPER_SERVERS)
+
+    waitingForFirstResponseFrom $all_zookeeper_servers
 else
     exec "$@"
 fi
-
-
